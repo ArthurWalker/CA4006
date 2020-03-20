a93a4ee2d8fce9b958c98d89951c4bc257e20400
ArthurWalker <phuc.phamhuu2@mail.dcu.ie>
2020-03-20 18:18:53 +0000

    Finish Queue

--- Workplan.java
+++ Workplan.java
@@ -8,22 +8,17 @@ import java.util.Queue;
 
 import CA4006.Generator;
 
-public class Workplan implements Runnable {
+import java.util.LinkedList;
 
+
+public class Workplan implements Runnable {
+    private LinkedList queue = new LinkedList();
 	private int[][] tasks;
 	private Integer numTask;
 	
 	public Workplan(Integer numTask) {
 		this.numTask= numTask;
 		generateTask();
-	}
-	
-	public void generateTask() {
-		int[][] temp = new int[numTask][];
-		for (int i = 0; i < numTask; i++) {
-			temp[i] = singleTask();
-		}
-		this.tasks = temp;
 	}
 
 	public int[][] getTasks() {
@@ -35,11 +30,32 @@ public class Workplan implements Runnable {
 		arr[0] = Generator.generateRobotID();
 		arr[1] = Generator.generateNumParts();
 		arr[2] = Generator.generateAircraftID();
-		System.out.println(Arrays.toString(arr));
 		return arr;
 	}
 
+	public void generateTask() {
+		int[][] temp = new int[numTask][];
+		for (int i = 0; i < numTask; i++) {
+			temp[i] = singleTask();
+			enqueue(temp[i]);
+		}
+		this.tasks = temp;
+	}
+	
+	public void enqueue(Object arr) {
+		this.queue.addLast(arr);
+	}
+	
+	public Object dequeue() {
+		return this.queue.removeFirst();
+	}
+	
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
+	
 	public void run() {
+		System.out.println(this.queue);
 		System.out.println("This is workplan in Thread " + Thread.currentThread().getName());
 	}
 
